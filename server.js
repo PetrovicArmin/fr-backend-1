433204966eb26077a75e006ffed04e72e2c34cf6
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-10-12 10:15:22 +0200

    change

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const db = knex(knexSettings);
 
 app.use(cors({origin: "https://fr-frontend-1.herokuapp.com"}));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
